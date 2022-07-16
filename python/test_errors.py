ee784714dbe412403736b81cdad9ca0da274fc76
Julien Depredurand <julien.depredurand@gmail.com>
2022-07-15 21:15:01 -0700

    added asset create test

--- python/test_errors.py
+++ python/test_errors.py
@@ -10,11 +10,20 @@ target="debug"
 # pwd is different inside/outside of Neovim when using Git
 pwd = re.sub("/python","",os.getcwd())
 command = pwd+"/target/"+target+"/tigershark2"
-print(">>" , command)
+
+
+json = '{\
+"name":"my new asset",\
+"location":"my_location",\
+"source":"my_source",\
+"datapath":"/my/data/path"}'
+
+# // Popen args
+c = [command,"-c","create","-a", json] # CREATE
 
 
 try:
-    process = Popen([command], stdout=PIPE)
+    process = Popen(c, stdout=PIPE)
     (output, err) = process.communicate()
     exit_code = process.wait()
     output = output.decode('utf-8')

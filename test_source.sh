069e4f279075807fb379ad4b01fc9f1078248774
Julien Depredurand <julien.depredurand@gmail.com>
2022-07-18 00:00:34 -0700

    added source test

---
+++ test_source.sh
@@ -0,0 +1,4 @@
+./target/debug/tigershark2 -c source -a '{
+"name":"my other asset",
+"location":"my_location",
+"version":12}'
---
+++ test_source2.sh
@@ -0,0 +1,3 @@
+./target/debug/tigershark2 -c source -a '{
+"name":"my other asset",
+"location":"my_location"}'

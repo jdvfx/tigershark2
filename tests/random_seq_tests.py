54dadad0d982d3df677cdd8e0bf63f699599373a
jdvfx_fedora36 <julien.depredurand@gmail.com>
2022-08-14 21:57:28 -0700

    cleanup tests

--- tests/random_seq_tests.py
+++ tests/random_seq_tests.py
@@ -12,16 +12,15 @@ exe = "release"
 ts_exec+="/target/"+exe+"/tigershark2"
 command = ts_exec
 
-# commands = ["create","latest","update","source","approve","delete"]
-commands = ["delete"]
-
+commands = ["create","latest","update","source","approve","delete"]
+commands.append("bullshit")
 
 
 # print in RED colors (for errors)
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 
 # return tuple with (ErrorCode,output)
-def db_insert(mycommand,myjson):
+def tigershark2(mycommand,myjson):
 
     try:
         process = Popen([command,"-c",mycommand,"-a",json.dumps(myjson)], stdout=PIPE)
@@ -36,9 +35,9 @@ def db_insert(mycommand,myjson):
     except:
         return (1,"Python Popen failed")
 
-asset_names = ["sphere","cube","cone","volume","point","torus","plane","grid","teapot","ball","ground","particle","mouse","bird","cat","dog","car"]
+asset_names = ["sphere","cube","cone","volume","point","torus","plane","grid","teapot","ball","ground","particle","mouse","bird","cat","dog","car","duck","tree","robot","droid","spaceship","banana","apple","orange","berry","peach","pizza","burger","fries","water","fire","Dust","Explosion","Dark Energy","MagicBeam","Lightning Bolts"]
 
-letters = "abcdefghijklmnopqrs"
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 for i in range(100):
@@ -46,15 +45,20 @@ for i in range(100):
     mycommand = random.choice(commands)
 
     randomchar = random.choice(letters)
-    a=  random.choice(asset_names) + "_" + randomchar
+    a =  random.choice(asset_names) + "_" + randomchar
     l = "/location/"+a
     s = "/source/"+a
     d = "/data/path/"+a
-    v = random.randint(0,10)
+    v = random.randint(0,20)
+
+
+    if random.random()>0.94:
+        l = "bullshit"
 
     myjson = {"name":a, "location":l,"source":s,"datapath":d, "version":v}
 
-    out = db_insert(mycommand,myjson)
+    # execute command with assetjson
+    out = tigershark2(mycommand,myjson)
 
     l1 = ">--- "+str(mycommand) +" "+str(myjson)
     if out[0]!=0:

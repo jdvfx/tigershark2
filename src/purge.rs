fdc73d266dfa5f51f03403e517e790febb23bc79
Julien Depredurand <julien.depredurand@gmail.com>
2022-08-18 22:56:22 -0700

    find all versions marked as Purge

--- src/purge.rs
+++ src/purge.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code, unused_variables, unused_assignments, unused_imports)]
 
+use mongodb::bson::doc;
 pub mod parse_args;
 use errors::{exit_or_panic, CliOutput};
 use parse_args::CommandType;
@@ -20,8 +21,15 @@ async fn main() {
     let collection = db::connect_to_db(uri, db_name, collection_name);
     match collection.await {
         Some(coll) => {
-            println!("{:?}", coll);
+            let filter: bson::Document = doc! { "versions": {"$elemMatch": { "status": "Purge"}}};
+            let mut cursor = coll.find(filter.clone(), None).await.unwrap();
+            while cursor.advance().await.unwrap() {
+                let c = cursor.deserialize_current();
+                println!("\n{:?}", c);
+            }
         }
         None => println!("no collections"),
     }
 }
+
+

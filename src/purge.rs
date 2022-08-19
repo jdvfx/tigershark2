f5a0b7b2c33abe0202bf7c44241a015c6a4ae81b
Julien Depredurand <julien.depredurand@gmail.com>
2022-08-18 20:50:43 -0700

    first test, just loads the collection

--- src/purge.rs
+++ src/purge.rs
@@ -1,5 +1,27 @@
-fn main(){
+#![allow(dead_code, unused_variables, unused_assignments, unused_imports)]
 
+pub mod parse_args;
+use errors::{exit_or_panic, CliOutput};
+use parse_args::CommandType;
 
-    println!("main PURGE");
+mod assetdef;
+pub mod db;
+pub mod errors;
+pub mod utils;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let cli_output: CliOutput;
+    let uri = "mongodb://localhost:27017";
+    let db_name = "sharks";
+    let collection_name = "tiger";
+
+    let collection = db::connect_to_db(uri, db_name, collection_name);
+    match collection.await {
+        Some(coll) => {
+            println!("{:?}", coll);
+        }
+        None => println!("no collections"),
+    }
 }

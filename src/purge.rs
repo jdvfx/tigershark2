72132cc4bcfc5ec0b1984729b42bf83b1bd1fa88
Julien Depredurand <julien.depredurand@gmail.com>
2022-08-18 23:26:25 -0700

    find all assets with status:Purge, get Documents, list all versions with name,version,status : need to get rid of unwrap()

--- src/purge.rs
+++ src/purge.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code, unused_variables, unused_assignments, unused_imports)]
 
+use bson::{de::Error, raw::ErrorKind};
 use mongodb::bson::doc;
 pub mod parse_args;
 use errors::{exit_or_panic, CliOutput};
@@ -9,6 +10,7 @@ mod assetdef;
 pub mod db;
 pub mod errors;
 pub mod utils;
+use assetdef::Asset;
 use std::env;
 
 #[tokio::main]
@@ -21,15 +23,30 @@ async fn main() {
     let collection = db::connect_to_db(uri, db_name, collection_name);
     match collection.await {
         Some(coll) => {
+            // let s = purge(coll);
+
             let filter: bson::Document = doc! { "versions": {"$elemMatch": { "status": "Purge"}}};
             let mut cursor = coll.find(filter.clone(), None).await.unwrap();
             while cursor.advance().await.unwrap() {
                 let c = cursor.deserialize_current();
-                println!("\n{:?}", c);
+                let name = &c.as_ref().unwrap().name;
+                for version in c.as_ref().unwrap().versions.iter() {
+                    let status = &version.status;
+                    println!("{:?} {:?} {:?}", name, version, status);
+                }
+                // println!("\n{:?}", c);
             }
         }
         None => println!("no collections"),
     }
 }
 
-
+// async fn purge(coll: mongodb::Collection<Asset>) -> Result<String, ErrorKind> {
+//     let filter: bson::Document = doc! { "versions": {"$elemMatch": { "status": "Purge"}}};
+//     let mut cursor = coll.find(filter.clone(), None).await?;
+//     while cursor.advance().await? {
+//         let c = cursor.deserialize_current();
+//         println!("\n{:?}", c);
+//     }
+//     Ok("S".to_string())
+// }
